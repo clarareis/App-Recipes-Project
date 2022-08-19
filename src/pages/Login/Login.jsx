891829e5933204f4da27478481e3966f11fe4f2b
Breno Nascimento <satsukebare23@gmail.com>
2022-08-19 17:29:31 -0300

    add auth form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [userData, setuserData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleUserData = ({ name, value }) => setuserData({ ...userData, [name]: value });
+
   return (
     <section>
-      <h1>Login</h1>
+      <form>
+        <label htmlFor="email">
+          <input
+            data-testid="email-input"
+            id="email"
+            placeholder="email"
+            onChange={ ({ target }) => handleUserData(target) }
+            name="email"
+            value={ userData.email }
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            data-testid="password-input"
+            id="password"
+            placeholder="password"
+            onChange={ ({ target }) => handleUserData(target) }
+            name="password"
+            value={ userData.password }
+          />
+        </label>
+        <button
+          type="button"
+        >
+          Enter
+        </button>
+      </form>
     </section>
   );
 }
