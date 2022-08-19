75a23ecb5dc924f8deb3d04359488d9cbdcd638b
Breno Nascimento <satsukebare23@gmail.com>
2022-08-19 16:48:14 -0300

    create rotes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,33 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import DoneRecipe from './pages/DoneRecipe/DoneRecipe';
+import Foods from './pages/Foods/Foods';
+import Drinks from './pages/Drinks/Drinks';
+import FoodRecipe from './pages/FoodRecipe/FoodRecipe';
+import DrinkRecipe from './pages/DrinkRecipe/DrinkRecipe';
+import profile from './pages/Profile/profile';
+import FavoriteRecipe from './pages/FavoriteRecipe/FavoriteRecipe';
+
+const id = 'id-da-receita';
 
 function App() {
   return (
     <main className="meals">
       <Switch>
-        <Route />
+        <Route component={ Login } path="/" exact />
+        <Route component={ Foods } path="/foods" exact />
+        <Route component={ Drinks } path="/drinks" exact />
+        <Route component={ FoodRecipe } path={ `/foods/${id}` } exact />
+        <Route component={ DrinkRecipe } path={ `/drinks/${id}` } exact />
+        <Route
+          component={ DoneRecipe }
+          path={ `/foods/${'id-da-receita'}/in-progress` }
+          exact
+        />
+        <Route component={ profile } path="/profile" exact />
+        <Route component={ FavoriteRecipe } path="/favorite-recipes" exact />
       </Switch>
     </main>
   );
