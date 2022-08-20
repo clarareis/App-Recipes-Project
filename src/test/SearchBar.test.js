041c3834abaa59653a3a6bcac0ad0b644119dad9
Breno Nascimento <satsukebare23@gmail.com>
2022-08-20 16:40:45 -0300

    100% of tests in

--- src/test/SearchBar.test.js
+++ src/test/SearchBar.test.js
@@ -3,7 +3,7 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { renderWithRouterAndRedux } from './RenderWith';
 import Header from '../Components/Header';
-import { ingredientMock } from '../../_Mocks/SearchMock';
+import { ingredientMock, nameMock } from '../../_Mocks/SearchMock';
 
 const searchInputId = 'search-input';
 const showSearchBtnId = 'search-top-btn';
@@ -12,10 +12,6 @@ const ingredientsBtnId = 'ingredient-search-radio';
 const nameBtnId = 'name-search-radio';
 const firsLetterBtnId = 'first-letter-search-radio';
 const searchBtnId = 'exec-search-btn';
-
-global.fetch = jest.fn(() => Promise.resolve({
-  json: () => Promise.resolve(ingredientMock),
-}));
 
 beforeEach(() => {
   fetch.mockClear();
@@ -43,6 +39,10 @@ describe('test in search component', () => {
     expect(searchInput).toBeInTheDocument();
   });
 
+  global.fetch = jest.fn(() => Promise.resolve({
+    json: () => Promise.resolve(ingredientMock),
+  }));
+
   test('fetch is calles', async () => {
     renderWithRouterAndRedux(<Header
       headerName="foods"
@@ -60,5 +60,53 @@ describe('test in search component', () => {
     userEvent.click(ingredientBtn);
     userEvent.click(searchBtn);
     expect(fetch).toHaveBeenCalledTimes(1);
+  });
+
+  global.fetch = jest.fn(() => Promise.resolve({
+    json: () => Promise.resolve(nameMock),
+  }));
+
+  test('test if meal api is caled in food area with name filter selected', () => {
+    renderWithRouterAndRedux(<Header
+      headerName="foods"
+    />);
+
+    const nameRadio = screen.getByTestId(nameBtnId);
+    const showSearch = screen.getByTestId(showSearchBtnId);
+    const searchBtn = screen.getByTestId(searchBtnId);
+
+    userEvent.click(showSearch);
+
+    const searchInput = screen.getByTestId(searchInputId);
+
+    userEvent.type(searchInput, 'Arrabiata');
+    userEvent.click(nameRadio);
+    userEvent.click(searchBtn);
+
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+
+  jest.spyOn(window, 'alert').mockImplementation(() => {});
+  test(`verify if alert is called case first latter filter
+    with active and input have more one latter`, async () => {
+    renderWithRouterAndRedux(
+      <Header
+        headerName="foods"
+      />,
+    );
+
+    const firstLatter = screen.getByTestId(firsLetterBtnId);
+    const showSearch = screen.getByTestId(showSearchBtnId);
+    const searchBtn = screen.getByTestId(searchBtnId);
+
+    userEvent.click(showSearch);
+
+    const searchInput = screen.getByTestId(searchInputId);
+
+    userEvent.type(searchInput, 'aaa');
+    userEvent.click(firstLatter);
+    userEvent.click(searchBtn);
+
+    expect(alert).toHaveBeenCalled();
   });
 });
