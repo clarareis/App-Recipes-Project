6eb3b19bda30ff7dd595dbe033deec699c6bc305
Breno Nascimento <satsukebare23@gmail.com>
2022-08-20 15:18:08 -0300

    add dynamic endpoint

--- src/services/_end_points.js
+++ src/services/_end_points.js
@@ -1,9 +1,24 @@
-export const INGREDIENT_ENDPOINT = (data) => `https://www.themealdb.com/api/json/v1/1/filter.php?i=${data}`;
-export const NAME_ENDPOINT = (data) => `https://www.themealdb.com/api/json/v1/1/search.php?s=${data}`;
-export const FIRST_LETTER_ENDPOINT = (data) => `https://www.themealdb.com/api/json/v1/1/search.php?f=${data}`;
+export const MEAL_INGREDIENT_ENDPOINT = (data) => `https://www.themealdb.com/api/json/v1/1/filter.php?i=${data}`;
+export const MEAL_NAME_ENDPOINT = (data) => `https://www.themealdb.com/api/json/v1/1/search.php?s=${data}`;
+export const MEAL_FIRST_LETTER_ENDPOINT = (data) => `https://www.themealdb.com/api/json/v1/1/search.php?f=${data}`;
 
-export const changeEndPointByFilter = (filter, data) => {
-  if (filter === 'ingredient') return INGREDIENT_ENDPOINT(data).toString();
-  if (filter === 'name') return NAME_ENDPOINT(data).toString();
-  if (filter === 'first-letter') return FIRST_LETTER_ENDPOINT(data).toString();
+export const DRINK_INGREDIENT_ENDPOINT = (data) => `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${data}`;
+export const DRINK_NAME_ENDPOINT = (data) => `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${data}`;
+export const DRINK_FIRST_LETTER_ENDPOINT = (data) => `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${data}`;
+
+const getMealEndpoints = (filter, data) => {
+  if (filter === 'ingredient') return MEAL_INGREDIENT_ENDPOINT(data).toString();
+  if (filter === 'name') return MEAL_NAME_ENDPOINT(data).toString();
+  if (filter === 'first-letter') return MEAL_FIRST_LETTER_ENDPOINT(data).toString();
+};
+
+const getDrinksEndpoints = (filter, data) => {
+  if (filter === 'ingredient') return DRINK_INGREDIENT_ENDPOINT(data).toString();
+  if (filter === 'name') return DRINK_NAME_ENDPOINT(data).toString();
+  if (filter === 'first-letter') return DRINK_FIRST_LETTER_ENDPOINT(data).toString();
+};
+
+export const changeEndPointByFilterOrPage = (page, filter, data) => {
+  if (page === 'foods') return getMealEndpoints(filter, data);
+  getDrinksEndpoints(filter, data);
 };
--- src/services/fetchFoodsAndDrinks.js
+++ src/services/fetchFoodsAndDrinks.js
@@ -1,15 +1,21 @@
-import { changeEndPointByFilter } from './_end_points';
+import { changeEndPointByFilterOrPage } from './_end_points';
 
-export const requestRecipesByfilter = async (filter, data) => {
+export const requestRecipesByfilter = async (page, filter, data) => {
   try {
-    const recipeApiResponse = await fetch(changeEndPointByFilter(filter, data));
-    const sufleRecipesRequest = recipeApiResponse.json();
-    return sufleRecipesRequest;
+    const recipeApiResponse = await fetch(changeEndPointByFilterOrPage(filter, data));
+    const successRecipesRequest = recipeApiResponse.json();
+    return successRecipesRequest;
   } catch (error) {
     console.log(error);
   }
 };
 
-export const requestRecipes = async () => {
-
+export const requestRecipes = async (page) => {
+  try {
+    const recipeApiResponse = await fetch(changeEndPointByFilterOrPage(page));
+    const sufleRecipesRequest = recipeApiResponse.json();
+    return sufleRecipesRequest;
+  } catch (error) {
+    console.log(error);
+  }
 };
