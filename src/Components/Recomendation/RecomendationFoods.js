bd40cf636f9649942670f75ca1de86ffcd7b528c
Rubio <paulocezarrubio@gmail.com>
2022-08-24 16:23:19 -0300

    resolvendo conflitos

--- src/Components/Recomendation/RecomendationFoods.js
+++ src/Components/Recomendation/RecomendationFoods.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './recomendation.css';
 
 function RecomendationFoods({ recomendation, i }) {
   const { strMeal } = recomendation;
