809175864498ecbdd9eb5c23d6e1db9c6af463f8
Laura Lana <lauramarqueslana@gmail.com>
2022-08-23 22:17:37 -0300

    teste

--- src/pages/DoneRecipe/DoneRecipe.jsx
+++ src/pages/DoneRecipe/DoneRecipe.jsx
@@ -6,38 +6,6 @@ function DoneRecipe() {
     <section>
       <Header headerName="Done Recipes" />
       <h1>DoneRecipe</h1>
-      {/* <button
-        data-testid="filter-by-all-btn"
-        type="button"
-      >
-        All
-      </button>
-      <button
-        data-testid="filter-by-food-btn"
-        type="button"
-      >
-        Foods
-      </button>
-      <button
-        data-testid="filter-by-drink-btn"
-        type="button"
-      >
-        Drinks
-      </button>
-      <img
-        data-testid={ `${index}-horizontal-image"` }
-        alt="card-img"
-      />
-      <h3 data-testid={ `${index}-horizontal-top-text` }>CATEGORIA</h3>
-      <h4 data-testid={ `${index}-horizontal-name` }>NOME</h4>
-      <p data-testid={ `${index}-horizontal-done-date` }>DATA</p>
-      <p data-testid={ `${index}-${tagName}-horizontal-tag` }>Receita</p>
-      <button
-        data-testid={ `${index}-horizontal-share-btn` }
-        type="button"
-      >
-        Share
-      </button> */}
     </section>
   );
 }
